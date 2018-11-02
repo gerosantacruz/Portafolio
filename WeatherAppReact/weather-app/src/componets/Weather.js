ca61bda8c74e2cb63babd972161091e68f9ecfc3
gerosantacruz <geronimo.santacruz2017@gmail.com>
2018-11-02 18:28:20 -0300

    css and bootrap

--- WeatherAppReact/weather-app/src/componets/Weather.js
+++ WeatherAppReact/weather-app/src/componets/Weather.js
@@ -3,14 +3,14 @@ import React from "react"
 class Weather extends React.Component{
     render(){
         return(
-            <div>
-                <p>Weather Information</p>
-                {this.props.city && <p>Location: {this.props.city}</p>}
-                {this.props.country &&<p>Country: {this.props.country}</p>}
-                {this.props.temperature && <p>Temperature: {this.props.temperature}</p>}
-                {this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
-                {this.props.description && <p>Conditions: {this.props.description}</p>}
-                {this.props.error && <p>{this.props.error}</p>}
+            <div ClassName="weather__info">
+                
+                {this.props.city && <p className="weather__key">Location: <span className="weather__value"> {this.props.city}</span> </p>}
+                {this.props.country &&<p className="weather__key">Country: <span className="weather__value">  {this.props.country}</span></p>}
+                {this.props.temperature && <p className="weather__key">Temperature: <span className="weather__value">{this.props.temperature}</span></p>}
+                {this.props.humidity && <p className="weather__key">Humidity: <span className="weather__value">{this.props.humidity}</span></p>}
+                {this.props.description && <p className="weather__key">Conditions: <span className="weather__value">{this.props.description}</span></p>}
+                {this.props.error && <p className="weather__key"> <span className="weather__value">{this.props.error}</span></p>}
             </div>
         )
     }
