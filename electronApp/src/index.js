de717c5d9efe388c500b5ef64057d91ca018bdb4
gerosantacruz <geronimo.santacruz2017@gmail.com>
2018-12-07 18:41:07 -0300

    Fix html bug in visual studio

--- electronApp/src/index.js
+++ electronApp/src/index.js
@@ -1,8 +1,17 @@
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, Menu} = require('electron');
 const url = require('url');
-const path = require('path')
+const path = require('path');
 
-let mainWindow
+if(process.eventNames.NODE_ENV !== 'production'){
+    require('electron-reload')(__dirname, {
+        electron: path.join(__dirname, '../node_modules', '.bin'
+        ,'electron')
+    })
+}
+
+
+let mainWindow;
+let newProductWindow;
 
 
 app.on('ready', ()=> {
@@ -12,4 +21,39 @@ app.on('ready', ()=> {
         protocol: 'file',
         slashes: true
     }))
+
+    const mainMenu= Menu.buildFromTemplate(templateMenu);
+    Menu.setApplicationMenu(mainMenu)
 });
+
+function createNewProductWindow(){
+    newProductWindow = new BrowserWindow({
+        width:400,
+        height:500,
+        title:'Add a new Product'
+    });
+    newProductWindow.setMenu(false);
+    newProductWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'views/newProduct.html'),
+        protocol: 'file',
+        slashes: true
+    }))
+
+}
+
+const templateMenu = [
+    {
+        label:'File',
+        submenu:[
+            {
+                label: 'New Product',
+                accelerator:'ctrl+N',
+                click(){
+                    createNewProductWindow()
+                }
+            }
+        ]
+        
+    }
+]
+   
